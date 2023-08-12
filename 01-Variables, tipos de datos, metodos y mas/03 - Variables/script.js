c5e4c287af12b217a8291d00510be2e3e67ca217
Luis Alejandro Pérez Rodríguez <luisalejandro.p.r@outlook.com>
2023-08-12 13:21:08 -0400

    tercera leccion

---
+++ 01-Variables, tipos de datos, metodos y mas/03 - Variables/script.js
@@ -0,0 +1,25 @@
+// Hay 3 formas de declarar una variable 
+// var,let,const (var y let son iguales pero var casi no se usa), const es para constantes
+
+let fristName = 'Luis Aljeandr'
+let lastName = 'Perez Rodriguez'
+
+console.log(fristName, lastName);
+
+//Naming Convention
+// - Only latters, numbers, underscores and dollar signs
+// - Can't start with a number
+
+// Multi-Word Formatting
+// firstName = camelCase
+// first_name = undersocre
+// FirstName = pascalCase
+// firstname = lowervase
+
+let age = 40
+console.log(age);
+
+//Declara multiple valores en uno linea
+let a, b, c;
+const d = 10, e = 20, f = 30;
+console.log(d, e, f);

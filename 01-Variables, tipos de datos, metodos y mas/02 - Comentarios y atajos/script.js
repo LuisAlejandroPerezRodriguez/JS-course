eec05c7caf41060e21bf09c279187922543a791d
Luis Alejandro Pérez Rodríguez <luisalejandro.p.r@outlook.com>
2023-08-12 12:26:25 -0400

    segunda leccion

---
+++ 01-Variables, tipos de datos, metodos y mas/02 - Comentarios y atajos/script.js
@@ -0,0 +1,12 @@
+// this is a single line of code
+//shift + arriba/abajo/derecha/izq (selecciono todo) 
+//CTRL + izquierda/derecha (me mueve de derecha a izquierda grande mente)
+//ALT + arriba/abajo (muevo lineas de codigos)
+//SHIFT + ALT +abajo/arriba (compio una linea de codigo)
+//CTRL + SHIFT + L (selecciono un varios pedazo de codigo y puedo editarlos )
+//CTRL +ALT + F (barra de busqueda)
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");

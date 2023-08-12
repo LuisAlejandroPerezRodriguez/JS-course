9a77934478ee45d7f83733aa7f74c16cafc701f6
Luis Alejandro Pérez Rodríguez <luisalejandro.p.r@outlook.com>
2023-08-12 17:08:44 -0400

    Leccion 6

---
+++ 01-Variables, tipos de datos, metodos y mas/06 - type conversion/script.js
@@ -0,0 +1,20 @@
+let amount = 99;
+
+//Convert string to numnber
+//amount = parseInt(amount);
+//amount = Number(amount);
+//amount = +amount;
+
+//Convert number to string
+//amount = amount.toString();
+//amount = String(amount);
+
+//Convert string to decimal
+//amount = parseFloat(amount);
+
+//Convert number to boolena
+amount = Boolean(amount);
+/* NaN significa not a number y sucede cuando 
+convertimos un string de caracteres a numero
+o alguna operacion nos da un numero imaginario*/
+console.log(amount, typeof amount);

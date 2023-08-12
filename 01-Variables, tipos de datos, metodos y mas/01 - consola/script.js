4dafb20b69e9fec5a516708a866eb4a17fa02be5
Luis Alejandro Pérez Rodríguez <luisalejandro.p.r@outlook.com>
2023-08-12 11:56:17 -0400

    Second commit

---
+++ 01-Variables, tipos de datos, metodos y mas/01 - consola/script.js
@@ -0,0 +1,13 @@
+console.log(100);
+console.log("klk", 20, true);
+
+const x = 25
+console.log(x + " veces")
+console.error("Bue")
+console.warn("Advertencia")
+console.table({ nombre: "fulano", correo: 'fulano@gmail.com' })
+
+console.group('simple')
+console.log(100);
+console.log("klk", 20, true);
+console.groupEnd();

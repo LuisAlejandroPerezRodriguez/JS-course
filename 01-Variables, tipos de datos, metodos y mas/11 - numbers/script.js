34546d398894f45e157e88a5732a91c69c8e87bb
Luis Alejandro Pérez Rodríguez <luisalejandro.p.r@outlook.com>
2023-08-13 20:26:49 -0400

    Leccion 11

---
+++ 01-Variables, tipos de datos, metodos y mas/11 - numbers/script.js
@@ -0,0 +1,11 @@
+let x;
+const num = new Number(5.2856);
+x = num.toString(); //Lo convierte a un string
+x = num.toString.length;
+x = num.toFixed(3);//Devuelve decimales
+x = num.toPrecision(4); // me ensena 4 numeros
+x = num.toExponential(2);
+x = num.toLocaleString('en-US')//Devuelve un string en el idioma que se indique 
+x = Number.MAX_VALUE; //Numero maximo que  soporta la varibales
+x = Number.MIN_VALUE;
+console.log(x);

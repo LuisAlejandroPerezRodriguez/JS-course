126ad15f1e08eccf0e128584b50555b273bf5ce7
Luis Alejandro Pérez Rodríguez <luisalejandro.p.r@outlook.com>
2023-08-14 17:55:24 -0400

    Se arreglo un bug

--- 01-Variables, tipos de datos, metodos y mas/15 - metodos de date/script.js
+++ 01-Variables, tipos de datos, metodos y mas/15 - metodos de date/script.js
@@ -3,10 +3,20 @@ let dt = new Date();
 
 x = dt.getFullYear(); // Me da el ano
 x = dt.getMonth() + 1; // Me da el mes
-x = dt.getDay() ; // Me dia de la semana
-x = dt.getDate() ; // Me dia del mes
+x = dt.getDay(); // Me dia de la semana
+x = dt.getDate(); // Me dia del mes
 
-x = Intl.DateTimeFormat('default').format(d); //donde va el default ponemos la zona donde nos encontemos en-US
-x = d.toLocaleString('default',{ month: 'long'});
+x = Intl.DateTimeFormat('default').format(dt); //donde va el default ponemos la zona donde nos encontemos en-US
+x = dt.toLocaleString('default', { month: 'long' });
 
+x = dt.toLocaleString('default', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: "numeric",
+    second: 'numeric',
+    timeZone: 'America/New_York',
+})
 console.log(x);

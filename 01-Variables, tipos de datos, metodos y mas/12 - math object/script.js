cce48d56766a5816caa089bce11222d3dcdc6dae
Luis Alejandro Pérez Rodríguez <luisalejandro.p.r@outlook.com>
2023-08-13 20:44:27 -0400

    Leccion 12

---
+++ 01-Variables, tipos de datos, metodos y mas/12 - math object/script.js
@@ -0,0 +1,13 @@
+let x;
+x = Math.sqrt(9); //raiz cuadrada
+x = Math.abs(-500) //valor absoluto
+x = Math.round(4.6)//redondea
+x = Math.ceil(6.2)//redonde siempre al numero de arriba
+x = Math.floor(4.9)//no redondea
+x = Math.pow(2, 3)//potencia 2^3
+x = Math.min(4, 5, 3);//numero minimo
+x = Math.max(4, 5, 3);//numero maximo
+x = Math.random();//Me dara un numero random entre 0 y 1
+x = Math.random() * 10; //Me da un numero random ente 0 y 9 con decimales
+x = Math.floor(Math.random() * 10 + 1); // Me da un numero random ente 1 y 10
+console.log(x);
